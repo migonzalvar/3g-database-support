7cc5b51501752061eae2723bfdf242d33900114e
Miguel Gonzalez <migonzalvar@gmail.com>
2013-06-26 18:07:03 +0200

    Remove previuos work

--- modemconfiguration/model.py
+++ modemconfiguration/model.py
@@ -3,30 +3,8 @@ import logging
 import os.path
 from xml.etree.cElementTree import ElementTree
 
-from gi.repository import Gtk
-
 from .config import COUNTRY_CODES_PATH, PROVIDERS_PATH, PROVIDERS_FORMAT_SUPPORTED
 # from .config import GSM_COUNTRY_PATH, GSM_PROVIDERS_PATH, GSM_PLAN_PATH
-
-
-def get_initial_rows():
-    return 2, 1, 0
-
-
-def get_initial_country():
-    # Check if persisted throw GConf GSM_COUNTRY_PATH
-    # If not guess using locale.getdefaultlocale()[0][3:5].lower()
-    return 2
-
-
-def get_initial_provider():
-    # Check if persisted throw GConf GSM_PROVIDERS_PATH
-    return 1
-
-
-def get_initial_plan():
-    # Check if persisted throw GConf GSM_PLAN_PATH
-    return 0
 
 
 class ServiceProviderDatabaseError(Exception):
@@ -179,7 +157,6 @@ class ServiceProvidersDatabase(object):
     def get_plans(self):
         return self._plans
 
-
     def get_country(self):
         return self._countries[self._current_country]
 
@@ -188,58 +165,3 @@ class ServiceProvidersDatabase(object):
 
     def get_plan(self):
         return self._plans[self._current_plan]
-
-
-class Plan2(list):
-    @property
-    def apn(self):
-        return "www.example.com"
-
-class Provider2(object):
-    def __init__(self, name):
-        self.name = name
-
-    @property
-    def plans(self):
-        plan_store = Gtk.ListStore(str, object)
-        for plan in plans[self.name]:
-            plan_store.append([plan, Plan(plan)])
-        return plan_store
-
-class Country2(object):
-    def __init__(self, name):
-        self.name = name
-
-    @property
-    def providers(self):
-        provider_store = Gtk.ListStore(str, object)
-        for provider in providers[self.name]:
-            provider_store.append([provider, Provider(provider)])
-        return provider_store
-
-plans = {
-    'Aaa': ['morning', 'afternoon'],
-    'Bbb': ['S', 'XL'],
-    'Zzz': ['base', 'red'],
-    'Yyy': ['combo', 'multi'],
-    '123': ['prepaid', 'postpaid'],
-    '987': ['basic', 'premium'],
-}
-
-providers = {
-    "Austria": ["Aaa", "Bbb"],
-    "Brazil": ["Zzz", "Yyy"],
-    "Belgium": ["123", "987"],
-}
-
-# countries = [
-#     Country("Austria"),
-#     Country("Brazil"),
-#     Country("Belgium"),
-# ]
-
-# class CountryListStore(Gtk.ListStore):
-#     def __init__(self, *args, **kwargs):
-#         Gtk.ListStore.__init__(self, str, object)
-#         for country in countries:
-#             self.append([country.name, country])
